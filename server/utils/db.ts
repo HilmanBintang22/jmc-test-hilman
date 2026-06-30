5ac05887866c4f5277a467244f80fcde498455bb
hilmanbintangardhani <hilmanbintang70@gmail.com>
2026-06-30 14:27:57 +0700

    fix db ts file for vercel

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,13 +1,13 @@
-import process from "node:process";
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
+  // Menggunakan parseInt untuk memastikan tipe datanya adalah angka (number) yang valid untuk TypeScript
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {}, // <--- UBAH BAGIAN INI: Cukup ketik {} untuk memaksa SSL aktif di serverless Vercel
+  ssl: {}, // Wajib objek kosong untuk enkripsi SSL Clever Cloud
   waitForConnections: true,
   connectionLimit: 10,
 });

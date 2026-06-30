7422ff43d553e133ffee4e1be466cc010680a938
hilmanbintangardhani <hilmanbintang70@gmail.com>
2026-06-30 14:16:33 +0700

    update env

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,14 +1,13 @@
 import process from "node:process";
 import mysql from "mysql2/promise";
 
-// Ubah menjadi 'export const db' agar Nitro bisa mendeteksi nama variabelnya secara otomatis
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306, // Ditambahkan || 3306 sebagai cadangan jika port kosong
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, // Diubah dari DB_PASS menjadi DB_PASSWORD agar lebih umum
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === "true" ? {} : undefined,
+  ssl: {}, // <--- UBAH BAGIAN INI: Cukup ketik {} untuk memaksa SSL aktif di serverless Vercel
   waitForConnections: true,
   connectionLimit: 10,
 });
